2f71fd73824c2d341f501dec6514b31a33b83383
shadishirinbeik <shadi.shirinbeik@gmail.com>
2026-09-17 14:28:02 +0200

    add syntax to create and load the dataset for airports table

--- sql/02_load_data.sql
+++ sql/02_load_data.sql
@@ -1,8 +1,6 @@
-/* ============================================================
-   Purpose : Load flights.csv into flights and check the load
-   DB      : PostgreSQL
-   ============================================================ */
-
+-- ------------------------------------------------------------
+-- Load flights.csv into flights and check the load
+-- ------------------------------------------------------------
 -- Change the path to where flights.csv is on your computer.
 COPY flights
 FROM 'C:/Users/Public/flights.csv'
@@ -14,3 +12,15 @@ SELECT
     MIN(flight_date) AS first_day,
     MAX(flight_date) AS last_day
 FROM flights;
+
+
+-- ------------------------------------------------------------
+-- Load airports.csv
+-- ------------------------------------------------------------
+-- Change the path to where flights.csv is on your computer.
+COPY airports
+FROM 'C:/Users/Public/airports.csv'
+WITH (FORMAT csv, HEADER true, NULL '');
+
+SELECT COUNT(*) AS total_airports
+FROM airports;
